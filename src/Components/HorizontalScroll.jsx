64ef3c9b9e568068bb8063276bff71378d27edc9
willGatos <masferrerw@gmail.com>
2023-03-05 03:59:31 -0500

    Starting

---
+++ src/Components/HorizontalScroll.jsx
@@ -0,0 +1,64 @@
+import React, { useRef, useState, useLayoutEffect, useCallback } from "react"
+import ResizeObserver from "resize-observer-polyfill"
+import {
+  motion,
+  useViewportScroll,
+  useTransform,
+  useSpring
+} from "framer-motion"
+
+const SmoothScroll = () => {
+  const scrollRef = useRef(null)
+  const ghostRef = useRef(null)
+  const [scrollRange, setScrollRange] = useState(0)
+  const [viewportW, setViewportW] = useState(0)
+
+  useLayoutEffect(() => {
+    scrollRef && setScrollRange(scrollRef.current.scrollWidth)
+  }, [scrollRef])
+
+  const onResize = useCallback(entries => {
+    for (let entry of entries) {
+      setViewportW(entry.contentRect.width)
+    }
+  }, [])
+
+  useLayoutEffect(() => {
+    const resizeObserver = new ResizeObserver(entries => onResize(entries))
+    resizeObserver.observe(ghostRef.current)
+    return () => resizeObserver.disconnect()
+  }, [onResize])
+
+  const { scrollYProgress } = useViewportScroll()
+  const transform = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, -scrollRange + viewportW]
+  )
+  const physics = { damping: 15, mass: 0.27, stiffness: 55 }
+  const spring = useSpring(transform, physics)
+
+  return (
+    <>
+      <div className="scroll-container">
+        <motion.section
+          ref={scrollRef}
+          style={{ x: spring }}
+          className="thumbnails-container"
+        >
+          <div className="thumbnails">
+            <div className="thumbnail" />
+            <div className="thumbnail" />
+            <div className="thumbnail" />
+            <div className="thumbnail" />
+            <div className="thumbnail" />
+            <div className="thumbnail" />
+          </div>
+        </motion.section>
+      </div>
+      <div ref={ghostRef} style={{ height: scrollRange }} className="ghost" />
+    </>
+  )
+}
+
+export default SmoothScroll
